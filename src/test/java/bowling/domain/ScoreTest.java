d66094a1c6a0dd891672f775d1ed6c876112b321
SONG22 <ansongi527@gmail.com>
2021-09-14 13:07:38 +0900

    fix(step3):

---
+++ src/test/java/bowling/domain/ScoreTest.java
@@ -0,0 +1,22 @@
+package bowling.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScoreTest {
+
+    @DisplayName(" ")
+    @Test
+    void pitch() {
+    }
+
+    @Test
+    void getScore() {
+    }
+
+    @Test
+    void canCalculateScore() {
+    }
+}
